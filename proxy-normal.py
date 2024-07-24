ac41dc29ebac4ecfbef7a29a0728496f6d66a7b2
Panurat <paunavt5@gmail.com>
2024-07-24 15:54:05 +0700

    add proxy

--- proxy-normal.py
+++ proxy-normal.py
@@ -1,11 +1,11 @@
 import os
 
 # กำหนดค่า proxy server
-os.environ['HTTP_PROXY'] = 'http://your.proxy.server:port'
-os.environ['HTTPS_PROXY'] = 'http://your.proxy.server:port'
+os.environ['HTTP_PROXY'] = 'http://192.168.0.135:6000'
+#os.environ['HTTPS_PROXY'] = 'http://your.proxy.server:port'
 
 # ตัวอย่างการใช้งาน
 import requests
 
-response = requests.get('http://example.com')
+response = requests.get('https://facebook.com')
 print(response.text)
